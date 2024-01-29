d173bfa8c12e97de93cf0c3e639774e9024caa9e
Wexop <mattheurtin@gmail.com>
2024-01-29 14:49:24 +0100

    annonce controller

---
+++ src/main/java/fr/mspr_java_b3/controllers/AnnonceController.java
@@ -0,0 +1,24 @@
+package fr.mspr_java_b3.controllers;
+
+import fr.mspr_java_b3.entities.Annonce;
+import fr.mspr_java_b3.entities.AnnonceEnum;
+import fr.mspr_java_b3.repository.AnnonceRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AnnonceController {
+
+    private final AnnonceRepository repository;
+
+    AnnonceController(AnnonceRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/annonce")
+    List<Annonce> getAnnonce() {
+        return repository.findAll().stream().filter(annonce -> annonce.getEtat() == AnnonceEnum.en_attente).toList();
+    }
+}
