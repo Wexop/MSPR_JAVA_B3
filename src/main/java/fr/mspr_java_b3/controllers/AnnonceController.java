4ee7c4f70b94af7bbcedcfc6f83bb41dc08c97e7
Wexop <mattheurtin@gmail.com>
2024-01-29 14:55:04 +0100

    route annonce ID

--- src/main/java/fr/mspr_java_b3/controllers/AnnonceController.java
+++ src/main/java/fr/mspr_java_b3/controllers/AnnonceController.java
@@ -4,6 +4,7 @@ import fr.mspr_java_b3.entities.Annonce;
 import fr.mspr_java_b3.entities.AnnonceEnum;
 import fr.mspr_java_b3.repository.AnnonceRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,4 +22,12 @@ public class AnnonceController {
     List<Annonce> getAnnonce() {
         return repository.findAll().stream().filter(annonce -> annonce.getEtat() == AnnonceEnum.en_attente).toList();
     }
+
+    @GetMapping("/annonce/{id}")
+    Annonce one(@PathVariable int id) {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new Error("Aucun utilisateur avec l'id " + id));
+    }
+
 }
