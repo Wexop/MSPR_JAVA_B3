44e5a19289400661897c32eb061c9aa74fc07b25
victor <victor.terrien@epsi.fr>
2024-01-29 17:08:53 +0100

    add CommentaireController

---
+++ src/main/java/fr/mspr_java_b3/controllers/CommentaireController.java
@@ -0,0 +1,27 @@
+package fr.mspr_java_b3.controllers;
+
+import fr.mspr_java_b3.entities.Commentaire;
+import fr.mspr_java_b3.repository.CommentaireRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CommentaireController {
+    private final CommentaireRepository repository;
+
+    CommentaireController(CommentaireRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping("/message")
+    Commentaire insertCommentaire(@RequestBody Commentaire commentaire) {
+        return repository.save(commentaire);
+    }
+
+    @GetMapping("/message/{id_article}")
+    List<Commentaire> getAllCommentaireById(@PathVariable("article_id") Integer id_article) {
+        return repository.findById(id_article).stream().toList();
+
+    }
+}
