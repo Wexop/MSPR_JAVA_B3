39dbac92f6113dcb7d33f65fb0766793c7d07b9f
Wexop <mattheurtin@gmail.com>
2024-03-12 12:25:06 +0100

    test docker no volume

--- src/test/java/fr/mspr_java_b3/MsprJavaB3ApplicationTests.java
+++ src/test/java/fr/mspr_java_b3/MsprJavaB3ApplicationTests.java
@@ -3,10 +3,8 @@ package fr.mspr_java_b3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MsprJavaB3ApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
