6d77803150dcdd0d47bc20720fa789f95610f342
Wexop <mattheurtin@gmail.com>
2024-05-24 10:28:58 +0200

    fix annonces get dates

--- src/main/java/fr/mspr_java_b3/dto/AnnonceGetDTO.java
+++ src/main/java/fr/mspr_java_b3/dto/AnnonceGetDTO.java
@@ -1,5 +1,6 @@
 package fr.mspr_java_b3.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import fr.mspr_java_b3.entities.Adresse;
 import fr.mspr_java_b3.entities.AnnonceEnum;
 import lombok.Getter;
@@ -18,8 +19,11 @@ public class AnnonceGetDTO {
     private Boolean besoin_aide;
     private String titre;
     private Adresse adresse;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime date_creation;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime date_debut;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime date_fin;
     private String utilisateur_nom;
     private PlanteGetDTO plante;
