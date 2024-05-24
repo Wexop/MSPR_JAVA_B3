80c84b52bc1ed39a60f5177fadd46e984fedee50
Wexop <mattheurtin@gmail.com>
2024-05-24 10:05:56 +0200

    fix plante get dto

--- src/main/java/fr/mspr_java_b3/dto/AnnonceGetDTO.java
+++ src/main/java/fr/mspr_java_b3/dto/AnnonceGetDTO.java
@@ -1,9 +1,12 @@
 package fr.mspr_java_b3.dto;
 
+import fr.mspr_java_b3.entities.Adresse;
 import fr.mspr_java_b3.entities.AnnonceEnum;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -14,4 +17,10 @@ public class AnnonceGetDTO {
     private String description;
     private Boolean besoin_aide;
     private String titre;
+    private Adresse adresse;
+    private LocalDateTime date_creation;
+    private LocalDateTime date_debut;
+    private LocalDateTime date_fin;
+    private String utilisateur_nom;
+    private PlanteGetDTO plante;
 }
