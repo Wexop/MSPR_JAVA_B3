e8d9b6d956173e72ec6278535cecd816a982116e
Wexop <mattheurtin@gmail.com>
2024-05-28 15:36:17 +0200

    clean imports

--- src/main/java/fr/mspr_java_b3/security/SpringdocConfig.java
+++ src/main/java/fr/mspr_java_b3/security/SpringdocConfig.java
@@ -1,10 +1,8 @@
 package fr.mspr_java_b3.security;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 
 @Configuration
 @SecurityScheme(
