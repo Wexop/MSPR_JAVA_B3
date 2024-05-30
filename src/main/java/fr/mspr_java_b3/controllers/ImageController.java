6c574ef40a7787037e13bab458bf4ef30fa37989
Wexop <mattheurtin@gmail.com>
2024-05-30 18:20:38 +0200

    add img bb key

--- src/main/java/fr/mspr_java_b3/controllers/ImageController.java
+++ src/main/java/fr/mspr_java_b3/controllers/ImageController.java
@@ -5,10 +5,8 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import fr.mspr_java_b3.controllers.responses.ImageResponse;
-import io.github.cdimascio.dotenv.Dotenv;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +22,7 @@ public class ImageController {
     @PostMapping(value = "/images", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     ImageResponse postImage(@RequestParam(name = "image") MultipartFile file) throws IOException, InterruptedException, UnirestException {
 
-        String url = String.format("https://api.imgbb.com/1/upload?key=%s", Dotenv.load().get("image_api_key"));
+        String url = String.format("https://api.imgbb.com/1/upload?key=%s", "368f4e7a790e6ddfd77a75fdf802a5bb");
 
         Unirest.setTimeouts(0, 0);
         HttpResponse<JsonNode> response = Unirest.post(url)
